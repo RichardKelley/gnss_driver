df08d0d4a5a76e26f43ac28a332ba0ba1eaf8b91
Richard Kelley <richard.kelley@gmail.com>
2017-09-05 11:59:02 -0700

    Added license info to source code.

--- include/util/macros.h
+++ include/util/macros.h
@@ -1,4 +1,23 @@
-// Copied from Apollo's GNSS driver.
+/******************************************************************************
+ * Original work Copyright 2017 The Apollo Authors. All Rights
+ * Reserved.  
+ * 
+ * Modified work Copyright 2017 Board of Regents of the
+ * Nevada System of Higher Education, on behalf of the University of
+ * Nevada, Reno. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 
 // Commonly-used macro definitions. Some of them are copied from
 // https://chromium.googlesource.com/chromium/src/base/+/master/macros.h
